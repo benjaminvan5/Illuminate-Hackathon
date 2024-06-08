a74e66e2b1b1c92e0c2143bd4d64567196480c5f
Alexander Chea <160830964+1alexc7@users.noreply.github.com>
2024-06-08 20:49:32 +1000

    test

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+with st.container():
+    st.subheader("subheading")
+    st.title("title")
+    st.write(
+        f"text"
+    )
+    st.write("[Learn More >](Hi")
