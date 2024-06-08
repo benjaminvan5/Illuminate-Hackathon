f4f053b482fa516fb9653cf99d721f58e340da5c
foemsy <128781244+foemsy@users.noreply.github.com>
2024-06-08 22:50:12 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import pickle
-import os
 
 st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
-
-dictionary = {}
-
 
 with st.container():
     st.subheader("Hi, I'm your digital medicine tracker :wave:")
@@ -16,15 +11,24 @@ with st.container():
     st.divider()
     medicine = st.text_input("What medicine are you currently taking?").lower()
     if len(medicine) > 0:
-        daily_dosage= st.text_input(f"What is your daily dosage of {medicine}?")
+        if "clear" in medicine:
+            with open('data.txt', 'w') as file:
+                pass
+        daily_dosage = st.text_input(f"What is your daily dosage of {medicine}?")
         if len(daily_dosage) > 0:
             st.write(f"Medicine: {medicine}. Daily Dosage: {daily_dosage}")
-            dictionary[medicine] = daily_dosage
-            st.write(dictionary)
+            with open('data.txt', 'a') as file:
+                file.write(f"(Medicine: {medicine}. Daily Dosage: {daily_dosage})  ")
     elif len(medicine) == 0:
         st.write("Medicine not entered.")
-        
-# line 32 thing makes nothing appear on screen
+
+    with open('data.txt', 'r') as file:
+        contents = file.read()
+        st.write(f"Text file contents: {contents}")
+
+
+
+
 placeholder = st.empty()
 with placeholder.container():
     st.write("test")
