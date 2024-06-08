baf8e9027202cf53fbbb503049b4137afd34ada5
benjaminvan5 <165342869+benjaminvan5@users.noreply.github.com>
2024-06-08 23:38:53 +1000

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+data = open('data.txt', 'r')
+medical_information = data.read()
+medical_information = medical_information.rstrip(medical_information[-1])
+dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
