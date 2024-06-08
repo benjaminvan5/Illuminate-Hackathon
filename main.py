85d6de32e0b4ff1196008c7eee0706426746a1ce
Alexander Chea <160830964+1alexc7@users.noreply.github.com>
2024-06-09 01:52:16 +1000

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ data = open('data.txt', 'r')
 
 with st.container():
     left_column, right_column  = st.columns((4,1))
+    
     with left_column:
         st.subheader("Hi, I'm your digital medicine tracker :wave:")
         st.title("MyMedMate")
         st.write("MyMedApp helps patients manage their medications by tracking dosages, and providing essential information on potential drug interactions.")
+    
     with right_column:
         file_ = open("apple.gif", "rb")
         contents = file_.read()
@@ -24,40 +26,43 @@ with st.container():
         )
 
 with st.container():
-    st.divider()
-    medicine = st.text_input("What medicine are you currently taking?").lower()
-    if len(medicine) > 0:
-        if "clear" in medicine:
-            st.write("Press Confirm if you want to clear your medicine and dosage information. Otherwise enter the medicine you are currently taking.")
-            clear_yes = st.button("Confirm")
-            if clear_yes:
-                with open('data.txt', 'w') as file:
-                    pass
-        else:
-            daily_dosage = st.text_input(f"What is your daily dosage of {medicine}?")
-            if len(daily_dosage) > 0:
-                if daily_dosage.isdigit():
-                    st.write(f"Medicine: {medicine}. Daily Dosage: {daily_dosage}")
-                    with open('data.txt', 'a') as file:
-                        file.write(f"({medicine}, {daily_dosage}),")
-                else:
-                    st.write("Please enter an integer")
-
+    left_column, right_column  = st.columns((1,1))
+    with left_column:
+        st.divider()
+        medicine = st.text_input("What medicine are you currently taking?").lower()
+        if len(medicine) > 0:
+            if "clear" in medicine:
+                st.write("Press Confirm if you want to clear your medicine and dosage information. Otherwise enter the medicine you are currently taking.")
+                clear_yes = st.button("Confirm")
+                if clear_yes:
+                    with open('data.txt', 'w') as file:
+                        pass
+            else:
+                daily_dosage = st.text_input(f"What is your daily dosage of {medicine}?")
+                if len(daily_dosage) > 0:
+                    if daily_dosage.isdigit():
+                        st.write(f"Medicine: {medicine}. Daily Dosage: {daily_dosage}")
+                        with open('data.txt', 'a') as file:
+                            file.write(f"({medicine}, {daily_dosage}),")
+                    else:
+                        st.write("Please enter an integer")
     
-    # Medicine input validation
-    elif len(medicine) == 0:
-        st.write("Medicine not entered.")
-    medical_information = data.read()
-    if medical_information != "":
-      medical_information = medical_information.rstrip(medical_information[-1])
-      dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
-    else: dictionary = {}
-    with open('data.txt', 'r') as file:
-        contents = file.read()
-        st.write(contents)
-        st.write(dictionary)
+        
+        # Medicine input validation
+        elif len(medicine) == 0:
+            st.write("Medicine not entered.")
+        medical_information = data.read()
+        if medical_information != "":
+          medical_information = medical_information.rstrip(medical_information[-1])
+          dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
+        else: dictionary = {}
+        with open('data.txt', 'r') as file:
+            contents = file.read()
+            st.write(contents)
+            st.write(dictionary)
 
-
+    with right_column:
+        st.write("text")
 
 
 placeholder = st.empty()
