5fc12454e1165a0aee9b4b3b2c27e799a4cc8462
foemsy <128781244+foemsy@users.noreply.github.com>
2024-06-08 21:08:50 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
 with st.container():
     st.subheader("Hi, I'm your digital medicine tracker :wave:")
     st.title("MyMedMate")
+    st.divider()
     st.write(
         f"text"
     )
