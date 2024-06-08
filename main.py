baff40786df17b115487b2e2c50996c7200888ad
Alexander Chea <160830964+1alexc7@users.noreply.github.com>
2024-06-08 21:10:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
 with st.container():
     st.subheader("Hi, I'm your digital medicine tracker :wave:")
     st.title("MyMedMate")
-    st.divider()
     st.write(
-        f"text"
+        "MyMedApp helps patients manage their medications by tracking dosages, and providing essential information on potential drug interactions."
     )
+    st.divider()
     st.write("I'm poopy ")
     
