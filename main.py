0b3e03e3253e0969b5bcfa89623cb2c6696b58e7
Alexander Chea <160830964+1alexc7@users.noreply.github.com>
2024-06-08 21:00:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+
 
 with st.container():
     st.subheader("subheading")
