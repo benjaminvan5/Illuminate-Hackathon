df604ecbc4e035c42ec5f194e0acf138a8718c2f
Alexander Chea <160830964+1alexc7@users.noreply.github.com>
2024-06-08 21:04:56 +1000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
 
 with st.container():
     st.subheader("subheading")
-    st.title("title")
+    st.title("MyMedMate")
     st.write(
         f"text"
     )
