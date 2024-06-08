93f9343b67ca02128f5a13ebd20f93f4d87acc39
benjaminvan5 <165342869+benjaminvan5@users.noreply.github.com>
2024-06-08 23:43:31 +1000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,7 @@
 data = open('data.txt', 'r')
 medical_information = data.read()
-medical_information = medical_information.rstrip(medical_information[-1])
-dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
+if medical_information == "":
+  pass
+else:
+  medical_information = medical_information.rstrip(medical_information[-1])
+  dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
