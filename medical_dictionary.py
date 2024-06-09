c587c716dcf5423464cd9c35bec8cc9ba58dd78d
benjaminvan5 <165342869+benjaminvan5@users.noreply.github.com>
2024-06-09 13:14:04 +1000

    Delete medical_dictionary.py

--- medical_dictionary.py
+++
@@ -1,7 +0,0 @@
-data = open('data.txt', 'r')
-global dictionary
-dictionary = {}
-medical_information = data.read()
-if medical_information != "":
-  medical_information = medical_information.rstrip(medical_information[-1])
-  dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
