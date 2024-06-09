a103ac21e986b52c528da9b496d301405fe8d6b6
foemsy <128781244+foemsy@users.noreply.github.com>
2024-06-10 00:17:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import base64
 st.set_page_config(page_title="Test Title", page_icon=":tada:", layout="wide")
 
 data = open('data.txt', 'r')
+dosages_data = open('dosages.txt', 'r')
 
-def clear_text(): # for clearing medicine and dosage
+def clear_text():  # for clearing medicine and dosage
     st.session_state.medicine = ""
     st.session_state.dosage = ""
 
+
 with st.container():
-    left_column, right_column  = st.columns((4,1))
-    
+    left_column, right_column = st.columns((4, 1))
+
     with left_column:
         st.subheader("Hi, I'm your digital medicine tracker :wave:")
         st.title("MyMedMate")
-        st.write("MyMedApp helps patients manage their medications by tracking dosages, and providing essential information on potential drug interactions.")
+        st.write(
+            "MyMedApp helps patients manage their medications by tracking dosages, and providing essential information on potential drug interactions.")
 
     # apple gif (an apple a day keeps the doctor away!) https://discuss.streamlit.io/t/how-to-show-local-gif-image/3408/4
     with right_column:
@@ -23,23 +26,23 @@ with st.container():
         contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
-        st.markdown(f'<img src="data:image/gif;base64,{data_url}" width="200">',unsafe_allow_html=True,)
+        st.markdown(f'<img src="data:image/gif;base64,{data_url}" width="200">', unsafe_allow_html=True, )
 
 with st.container():
     global file
     st.divider()
-    left_column, middle_column, right_column  = st.columns((2,0.7,2))
+    left_column, middle_column, right_column = st.columns((2, 0.7, 2))
     with left_column:
         medicine = st.text_input("What medicine are you currently taking?", key="medicine").lower()
         if len(medicine) > 0 and not ("," in medicine):
 
-        #radio buttons
+            # radio buttons
             medication_form_name = ['Tablet', 'Liquid', 'Capsule', 'Topical']
-            medication_form = st.radio('Form of Medication', medication_form_name, index = None)
+            medication_form = st.radio('Form of Medication', medication_form_name, index=None)
             with middle_column:
-                    st.write('#') #blank text to make units of medication aligned with user input
-                    st.write('#')
-                    st.write('#')
+                st.write('#')  # blank text to make units of medication aligned with user input
+                st.write('#')
+                st.write('#')
 
             if medication_form:
                 if medication_form == "Tablet":
@@ -71,29 +74,30 @@ with st.container():
                             file.write(f"({medicine}, {daily_dosage}),")
                     else:
                         st.write("Please enter an integer")
-    
-        
+
+
         # Medicine input validation
         elif len(medicine) == 0 or ',' in medicine:
             st.write("Invalid Medicine Name.")
         medical_information = data.read()
 
-        # Converts each (medicine, dosage) pair into a key-value element in a dictionary 
+        # Converts each (medicine, dosage) pair into a key-value element in a dictionary
         if medical_information != "":
-          medical_information = medical_information.rstrip(medical_information[-1])
-          dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
-        else: dictionary = {}
+            medical_information = medical_information.rstrip(medical_information[-1])
+            dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
+        else:
+            dictionary = {}
         with open('data.txt', 'r') as file:
             contents = file.read()
             st.write(contents)
             st.write(dictionary)
 
 
-
     # Function for updating dosage to a text file
     def update_dosage():
         with open('dosages.txt', 'a') as file:
-            file.write(f"({medicine.lower()}, {dosages}),") #buttons is the number on the right that you increase/decrease on the website
+            file.write(
+                f"({medicine.lower()}, {dosages}),")  # buttons is the number on the right that you increase/decrease on the website
         pass
 
 
@@ -103,34 +107,36 @@ with st.container():
             st.subheader("Daily Tracker 📅")
             count = 0
             for medicine in dictionary:
-                medicine = medicine[0].upper() + medicine[1:] # capatalises 1st letter of medicine
-                left, right = st.columns((4.9, 0.7)) # column for medicine name and amount of dose taken today
-               
+                medicine = medicine[0].upper() + medicine[1:]  # capatalises 1st letter of medicine
+                left, right = st.columns((4.9, 0.7))  # column for medicine name and amount of dose taken today
+
                 with left:
                     st.write(f"**{medicine}**")
-                    
-                col1, col2, col3 = st.columns((1.4, 3.5, 0.7)) # columns for user input, progess bar and metric
-                
+
+                col1, col2, col3 = st.columns((1.4, 3.5, 0.7))  # columns for user input, progess bar and metric
+
                 with col1:
-                    dosages = st.number_input("test", step = 1, label_visibility = "collapsed", key = f'{count}', min_value = 0, on_change=update_dosage)
-                 
+                    dosages = st.number_input("test", step=1, label_visibility="collapsed", key=f'{count}', min_value=0,
+                                              on_change=update_dosage)
+
                 with col2:
                     if dosages <= int(dictionary[medicine.lower()]):
                         progress_bar = col2.progress(dosages / int(dictionary[medicine.lower()]))
                     else:
-                        progress_bar = col2.progress(100) # max value of progress bar is 100
-                
+                        progress_bar = col2.progress(100)  # max value of progress bar is 100
+
                 with col3:
                     percent_increase = str(round(dosages / int(dictionary[medicine.lower()]) * 100)) + "%"
-                    col3.metric(label = "secret", value = f"{dictionary[medicine.lower()]}", delta = percent_increase, label_visibility = "collapsed")
+                    col3.metric(label="secret", value=f"{dictionary[medicine.lower()]}", delta=percent_increase,
+                                label_visibility="collapsed")
                     st.write(f"Medicine: {medicine}. Dosages: {dosages} - debug purposes")
-                    
+
                 with right:
                     st.write(f"**{dosages}**")
 
-                count += 1  
-         
-            #Clear button logic
+                count += 1
+
+                # Clear button logic
             global clear_button
             clear_button = st.button("Clear all data")
             if clear_button:
@@ -147,19 +153,20 @@ with st.container():
                             pass
                         st.rerun()
 
-                
+
                 show_cleardialog()
-
-
+        dosages_information = dosages_data.read()
         # Dictionary to store dosages
-        if medical_information != "":
-          medical_information = medical_information.rstrip(medical_information[-1])
-          dosages_dictionary = dict([x.split(',') for x in medical_information[1:-1].split('),(')])
-        else: dosages_dictionary = {}
+        if dosages_information != "":
+            dosages_information = dosages_information.rstrip(dosages_information[-1])
+            dosages_dictionary = dict([x.split(',') for x in dosages_information[1:-1].split('),(')])
+        else:
+            dosages_dictionary = {}
         with open('dosages.txt', 'r') as file:
             dosages_contents = file.read()
             st.write(dosages_contents)
             st.write(dosages_dictionary)
+
 placeholder = st.empty()
 with placeholder.container():
     st.write("test")
